10109f955f3f55401e15f09b4fea73ccc6e94da3
AdityaKumarv9h7 <122166694+AdityaKumarv9h7n@users.noreply.github.com>
2026-03-29 13:43:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -133,7 +133,7 @@ app.post('/api/download', (req, res) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
